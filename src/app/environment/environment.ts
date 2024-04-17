814d0aef2c01cdc8a7983aaf015ffa6a75cdfacb
ravikrpandey <ravi.pandey.iet@gmail.com>
2024-04-17 21:24:29 +0530

    2nd build

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   hmr: true,
   // serverUrl: 'http://192.168.39.89:8000',
-  serverUrl: 'https://905d-2409-40d0-5-85fb-e177-736-3cee-3f0.ngrok-free.app',
+  serverUrl: 'https://clean-pink-sheath-dress.cyclic.app/',
   defaultLanguage: 'Label',
   supportedLanguages: ['Label', 'TMSA'],
 };
