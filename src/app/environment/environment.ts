19a57af1868ebb74f22f7ea5d821d0ec198334da
Your Name <ravipandey@elitemindz.co>
2024-07-13 16:28:29 +0530

    updated build

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -3,7 +3,8 @@ export const environment = {
   hmr: true,
   // serverUrl: 'http://192.168.137.1:5000',
   // serverUrl: 'https://clean-pink-sheath-dress.cyclic.app/',
-  serverUrl: 'https://sarmusic.onrender.com',
+  // serverUrl: 'https://sarmusic.onrender.com',
+  serverUrl: 'https://api.sarmusic.site',
   defaultLanguage: 'Label',
   supportedLanguages: ['Label', 'TMSA'],
 };
